611af124b9c1be404fb1939a667fb6daa3c5a0d1
vm@wisc.edu <vm@wisc.edu>
2017-11-28 23:45:03 +0000

    Added run script, and updated spark context to use more mem.

---
+++ runApp1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ~/run.sh
+
+./sbt.sh package
+spark-submit --class "PageRankGraphX" --master spark://10.254.0.177:7077 ./target/scala-2.11/pagerankgraphx_2.11-1.0.jar
--- src/main/scala/PageRankGraphX.scala
+++ src/main/scala/PageRankGraphX.scala
@@ -1,21 +1,45 @@
 import org.apache.spark._
-//import org.apache.spark.graphx._
-//import org.apache.spark.rdd.RDD
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 
 object PageRankGraphX {
   def main(args: Array[String]) {
+    val graphFile = "/assignment3/soc-LiveJournal1.txt" // Should be some file on your system
+    val spark = SparkSession.builder.appName("PageRankGraphX Application")
+    	.config("spark.driver.memory","4g")
+	.config("spark.executor.memory","4g")
+	.config("spark.executor.cores","4")
+	.config("spark.driver.cores","4")
+	.config("spark.task.cpus", "1")
+    	.getOrCreate()
+   
+
+    // Read and filter comments from graph file.
+//    val unfilteredGraphData = spark.read.textFile(graphFile).cache()
+//    val graphData = unfilteredGraphData.filter(line => !line.contains("#"))
+    // Build graph verts and edges.
+//    val uniqueNodeIds = graphData.flatmap(line => line.split("\\W+")).distinct()
+//    val nodes: RDD[(VertexId, Double)] = uniqueNodeIds.map(id => (id, 0))
+//    val edges: RDD[Edge[Double]] = graphData.map(line => (line.split("\\W+")(0), line.split("\\W+")(1), 0))
+
+     val graphData = GraphLoader.edgeListFile(spark.sparkContext, graphFile)
+
+    /*
+     For i in 0 to 19 (20 iterations)
+     	 Aggregate messages and calculate value at each vertex.
+    */
+
+    println("Loaded graphData object!")
 
 
-    val logFile = "/wcount_input.txt" // Should be some file on your system
-    val spark = SparkSession.builder.appName("PageRankGraphX Application").getOrCreate()
 
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
-    spark.stop()
+    
+
+
+
+//    spark.stop()
   }
 }
 
