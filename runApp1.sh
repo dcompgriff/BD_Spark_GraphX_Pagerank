533ac4b89d120282eacefcc82255311874480de4
vm@wisc.edu <vm@wisc.edu>
2017-11-29 12:59:14 +0000

    Generated machine usage stats for graphx pagerank.

--- runApp1.sh
+++ runApp1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source ~/run.sh
-
+hadoop fs -rmr /tmp/ranks.csv
 ./sbt.sh package
 spark-submit --class "PageRankGraphX" --master spark://10.254.0.177:7077 ./target/scala-2.11/pagerankgraphx_2.11-1.0.jar
