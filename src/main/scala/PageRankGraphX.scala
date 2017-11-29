32166aedd51e3203cf54f63945058d2b4e7f4ef6
vm@wisc.edu <vm@wisc.edu>
2017-11-29 02:17:28 +0000

    Page rank correct implementation.

--- src/main/scala/PageRankGraphX.scala
+++ src/main/scala/PageRankGraphX.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.SparkSession
 
 object PageRankGraphX {
   def main(args: Array[String]) {
+    //val graphFile = "/assignment3/test-soc-LiveJournal1.txt" // Should be some file on your system
     val graphFile = "/assignment3/soc-LiveJournal1.txt" // Should be some file on your system
     val spark = SparkSession.builder.appName("PageRankGraphX Application")
     	.config("spark.driver.memory","4g")
@@ -24,22 +25,49 @@ object PageRankGraphX {
 //    val nodes: RDD[(VertexId, Double)] = uniqueNodeIds.map(id => (id, 0))
 //    val edges: RDD[Edge[Double]] = graphData.map(line => (line.split("\\W+")(0), line.split("\\W+")(1), 0))
 
-     val graphData = GraphLoader.edgeListFile(spark.sparkContext, graphFile)
+    val graphData = GraphLoader.edgeListFile(spark.sparkContext, graphFile)
+    val damping = 0.15
 
     /*
-     For i in 0 to 19 (20 iterations)
-     	 Aggregate messages and calculate value at each vertex.
+     Create the initial graph. Vertex values will be the current node's rank, and 
+     edge values will be one over the link factor, or the degree.
     */
 
-    println("Loaded graphData object!")
+    // Construct an output degree vertex graph.
+    val outDeg: VertexRDD[Int] = graphData.outDegrees
+    val degreeGraph = graphData.outerJoinVertices(outDeg) { (id, attr, outDegProp) => outDegProp match {
+     	case Some(outDeg) => outDeg
+	case None => 0}
+    }
 
+    // Create graph with each vertex mapped to it's out degree.
+    val inputGraph: Graph[Int, Int] = degreeGraph.outerJoinVertices(degreeGraph.outDegrees)((vid, _, degOpt) => degOpt.getOrElse(0))
+    // Create graph with edges mapped to one over the source vertex degree.
+    val initialPRGraph: Graph[Double, Double] = inputGraph.mapTriplets(triplet => 1.0 / triplet.srcAttr).mapVertices((id, _) => 1.0).cache()
 
+    /*
+    Create the functions necessary for the Pregel API.
+    */
+    // The initial message received by all vertices in PageRank
+    val initialRankSum = 0.0
 
+    // Define the message aggregator for aggregating messages.
+    def messageAggregator(rank1: Double, rank2: Double): Double = rank1 + rank2
+
+    // Define the vertex function for setting the node rank.
+    def vertexFunction(vid: VertexId, vattr: Double, sum: Double): Double = damping + ((1.0-damping)*sum)
     
+    // Define the message function for sending messages.
+    def sendMessage(edgeTup: EdgeTriplet[Double, Double]) = Iterator((edgeTup.dstId, edgeTup.srcAttr * edgeTup.attr))
 
+    // Execute pregel for a fixed number of iterations.
+    val finalPRGraph = Pregel(initialPRGraph, initialRankSum, 20, activeDirection = EdgeDirection.Out)(vertexFunction, sendMessage, messageAggregator)
 
+    // Get the final page ranks as an RDD.
+    val finalRanksRDD = finalPRGraph.vertices.map(vertexTup => vertexTup.toString)
+    finalRanksRDD.saveAsTextFile("/tmp/ranks.csv")
 
-//    spark.stop()
+    spark.stop()
   }
 }
 
